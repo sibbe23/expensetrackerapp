e012433c65324a8f1d2b7984d1b20f03033dcb32
sibbe23 <sibbe23@gmail.com>
2023-03-22 17:34:44 +0530

    Completed Delete functionality

--- controller/expense.js
+++ controller/expense.js
@@ -9,27 +9,32 @@ function stringvalidator(string){
     else return false
 }
 
-const addexpense = (req,res)=>{
+const addexpense = async(req,res)=>{
+    const t = await sequelize.transaction()
     const {expenseamount,description,category} = req.body;
     if(stringvalidator(expenseamount) || stringvalidator(description) || stringvalidator(category))
     {
         return res.status(400).json({err:'Fields left empty'})
     }
 
-    Expense.create({expenseamount,description,category,userId:req.user.id}).then(expense =>{
+    Expense.create({expenseamount,description,category,userId:req.user.id},{transaction:t}).then(expense =>{
         const totalExpense = Number(req.user.totalExpenses) + Number(expenseamount)
         console.log(totalExpense)
         User.update({totalExpenses:totalExpense},{
-            where:{id:req.user.id}
+            where:{id:req.user.id},
+            transaction:t
         }) .then(async()=>{
-                 res.status(200).json({expense:expense})
+            await t.commit();
+                return res.status(200).json({expense:expense})
             })
             .catch(async(err)=>{
+                await t.rollback()
                 return res.status(500).json({success:false,error:err})
             })
         
     })
-    .catch(err=>{
+    .catch(async(err)=>{
+        await t.rollback()
         console.log(err)
         return res.status(500).json({success :false,error:err})
     })
@@ -46,20 +51,28 @@ const getexpenses = (req,res)=>{
     })
 }
 
-const deleteexpense = (req,res)=>{
- const expenseid = req.params.expenseid;
- if(stringvalidator(expenseid)){
-  return  res.status(400).json({success:false,message:"Id not found"})
- }
- Expense.destroy({where:{id:expenseid,userId:req.user.id}})
- .then(()=>{
-    return res.status(200).json({success:true,message:"Delete Successful"})
- })
- .catch(err=>{
-    console.log(err);
-    return res.status(500).json({success:true,message:"failed"})
- })
-}
+const deleteexpense = async(req,res)=>{
+    const t = await sequelize.transaction()
+    const expenseid = req.params.expenseid;
+    if(stringvalidator(expenseid)){
+     return  res.status(400).json({success:false,message:"Id not found"})
+    }
+    const expense = await Expense.findByPk(expenseid, { transaction: t });
+    const users = await User.findByPk(req.user.id, { transaction: t });
+    users.totalExpenses -= expense.expenseamount;
+    await t.commit()
+    await users.save()
+
+
+    Expense.destroy({where:{id:expenseid,userId:req.user.id}})
+    .then(()=>{
+       return res.status(200).json({success:true,message:"Delete Successful"})
+    })
+    .catch(err=>{
+       console.log(err);
+       return res.status(500).json({success:true,message:"failed"})
+    })
+   }
 
 module.exports = {
     addexpense,
